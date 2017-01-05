bef5accfa36fb653609ab97516bc1ab083f66b65
Yunhsiu <anita8004@gmail.com>
2017-01-06 04:32:20 +0800

    variable
    php 變數練習

--- php/index.php
+++ php/index.php
@@ -2,17 +2,5 @@
 
   echo "Hellow World!!";
 
-  echo "<br/>";
-
-  $name = "Anita";
-  //直接用$設定變數
-
-  echo "My name is $name !";
-  //變數可直接放入字串
-
-  echo "<br/>";
-
-  echo "My name is ".$name;
-  // . 等於javascript裡的+
 
 ?>
---
+++ php/variable.php
@@ -0,0 +1,14 @@
+<?php
+
+  $name = "Anita";
+  //直接用$設定變數
+
+  echo "My name is $name !";
+  //變數可直接放入字串
+
+  echo "<br/>";
+
+  echo "My name is ".$name;
+  // . 等於javascript裡的+
+
+?>
